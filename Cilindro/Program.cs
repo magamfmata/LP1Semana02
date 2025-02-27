b465843747d60fb1afd98d78c02630a8a8248c62
Magali <magamayrmata@gmail.com>
2025-02-27 20:57:23 +0000

    Second commit

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -7,6 +7,23 @@ namespace Cilindro
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
+
+            Console.WriteLine("Insira a altura do Cilindro:");
+            string a = Console.ReadLine();
+
+            Console.WriteLine("Insira o raio do Cilindro:");
+            string r = Console.ReadLine();
+
+            double a_convert = double.Parse(a);
+            double r_convert = double.Parse(r);
+            double pi = Math.PI;
+
+            double V = a_convert *Math.Pow(r_convert,2) * pi;
+            string c = $"{V:f3}";
+            Console.WriteLine (c);
+
+
+
         }
     }
 }
