36e426423b0e46fb3c825c3f62579eb5315ffea4
Magali <magamayrmata@gmail.com>
2025-02-27 19:50:59 +0000

    First commit

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+            Console.WriteLine("Insira um número inteiro: ");
+
+            string a = Console.ReadLine();
+
+            sbyte n = sbyte.Parse(a);
+
+            Console.WriteLine(--n);
+            Console.WriteLine(++n);
+
+            
+
+        }
+    }
+}
