7f2c54c75ec4bbf0b64e64b5abf5c2e1a2077ed0
Magali <magamayrmata@gmail.com>
2025-02-28 17:55:37 +0000

    FInal commit

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Diagnostics.Tracing;
+using System.Globalization;
+using System.Net;
+using System.Threading;
 
 namespace GuessTheNumber
 {
@@ -6,7 +10,67 @@ namespace GuessTheNumber
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Random rnd = new Random();
+            int numberToGuess = rnd.Next(31);
+
+            for (int a = 1 ;; a++);
+
+            Console.WriteLine("Insert number:");
+
+             {
+                string numbertoguess, response;
+
+                Console.Write("Insert number: ");
+                numbertoguess = Console.ReadLine();
+
+                int guessednumber = int.Parse(numberToGuess);
+
+                if (guessednumber > 30 || guessednumber < 0)
+                {
+                    response = $"Numbers must be between 0 and 30. Try again.";
+                    Console.WriteLine(response);
+                    continue;
+                }
+
+                if (guessednumber > numberToGuess)
+                {
+                    response = $"The hidden number is lower than 
+                    {guessednumber}. Try Again.";
+
+                    Console.WriteLine(response);
+                    continue;
+                }
+                if (guessednumber < numberToGuess)
+                {
+                    response = $"The hidden number is higher than {guessednumber}
+                    . Try Again.";
+                    Console.WriteLine(response);
+                    continue;
+                }
+
+                if (guessednumber == numberToGuess)
+                {
+                    response = $"You found the hidden number {numberToGuess}
+                     after {Count} tries.";
+                     
+                    Console.WriteLine(response);
+                    break;
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+        
+                
+
+
+            
         }
     }
 }
